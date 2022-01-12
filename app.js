92f1287eb804fc7689f8b912ef29ed8263f4bba9
baileyalo <90293555+baileyalo@users.noreply.github.com>
2022-01-11 22:55:28 -0500

    lab

--- app.js
+++ app.js
@@ -6,14 +6,50 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        
-var router = express.Router();            
+var router = express.Router();  
 
+//meta data
+const NFT =   [
+    {
+        attributes: [
+          {
+            trait_type: "Breed",
+            value: "Maltipoo"
+          },
+          {
+            trait_type: "Eye color",
+            value: "Mocha"
+          }
+        ],
+        description: "The world's greatest Shinobi.",
+        image: "https://gateway.pinata.cloud/ipfs/Qmcy1NtKnpeduKDFXjLGHnqTa6kN4tzQ8ekUsMVHysf8Qj",
+        name: "Madara"
+      }
+]      
+
+
+
+//routes
 
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.send('hooray! welcome to our api!' );
+});
+router.get('/nft', function(req, res) {
+    console.log(NFT);
+    res.send(NFT);
 });
 
+router.post('/nft', function(req, res) {
+    
+    var nft = req.body;
+    console.log (req.body);
+
+    NFT.push(nft);
+
+    res.send('NFT Metadata added');
+});
 app.use('/api', router);
+
 
 app.listen(port);
 console.log('listening on port ' + port);
